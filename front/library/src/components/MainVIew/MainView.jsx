361844a3a4cce8821b4caf51952ca5672864be18
Ronen Metelitsa <ronenB>
2024-06-02 00:42:05 +0300

    fix: fixed search bar

--- front/library/src/components/MainVIew/MainView.jsx
+++ front/library/src/components/MainVIew/MainView.jsx
@@ -1,5 +1,5 @@
 // App.js
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -14,12 +14,25 @@ import { GlobalContext } from "../../contexts/GlobalContext";
 function MainView() {
   const [searchQuery, setSearchQuery] = useState("");
   const { books, setBooks } = useContext(GlobalContext);
+  const [filteredBooks, setFilteredBooks] = useState(books);
   const [openAddModal, setOpenAddModal] = useState(false);
   const [selectedBook, setSelectedBook] = useState(null);
   const [openDetailsModal, setOpenDetailsModal] = useState(false);
 
+  useEffect(() => {
+    setFilteredBooks(books);
+  }, [books]);
+
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
+    setFilteredBooks(
+      books.filter(
+        (book) =>
+          book.name.includes(event.target.value) ||
+          book.author.includes(event.target.value) ||
+          book.genre.includes(event.target.value)
+      )
+    );
   };
 
   const handleAddBook = (newBook) => {
@@ -45,68 +58,68 @@ function MainView() {
   };
 
   return (
-      <Box sx={{ bgcolor: "background.paper", height: "100%", py: 4 }}>
-        <Container maxWidth="lg" sx={{ flex: "1" }}>
-          <Grid
-            container
-            justifyContent="center"
-            spacing={2}
-            sx={{ height: "100%" }}
-          >
-            <Grid item xs={12} md={8}>
-              <SearchBar value={searchQuery} onChange={handleSearchChange} />
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              md={8}
-              sx={{ height: "80vh", overflowY: "scroll" }}
-            >
-              <BooksDisplay
-                books={books}
-                onBookClick={handleOpenDetailsModal}
-              />
-            </Grid>
-            <Grid item xs={12} md={8}>
-              <Button
-                onClick={handleOpenAddModal}
-                variant="contained"
-                color="primary"
-              >
-                Add Book
-              </Button>
-            </Grid>
+    <Box sx={{ bgcolor: "background.paper", height: "100%", py: 4 }}>
+      <Container maxWidth="lg" sx={{ flex: "1" }}>
+        <Grid
+          container
+          justifyContent="center"
+          spacing={2}
+          sx={{ height: "100%" }}
+        >
+          <Grid item xs={12} md={8}>
+            <SearchBar value={searchQuery} onChange={handleSearchChange} />
           </Grid>
-        </Container>
-        <AddBookModal
-          open={openAddModal}
-          onClose={handleCloseAddModal}
-          onAdd={handleAddBook}
-          propNames={[
-            "name",
-            "author",
-            "genre",
-            "page count",
-            "published date",
-            "current page",
-            "reading status",
-          ]}
+          <Grid
+            item
+            xs={12}
+            md={8}
+            sx={{ height: "80vh", overflowY: "scroll" }}
+          >
+            <BooksDisplay
+              books={filteredBooks ? filteredBooks : books}
+              onBookClick={handleOpenDetailsModal}
+            />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Button
+              onClick={handleOpenAddModal}
+              variant="contained"
+              color="primary"
+            >
+              Add Book
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+      <AddBookModal
+        open={openAddModal}
+        onClose={handleCloseAddModal}
+        onAdd={handleAddBook}
+        propNames={[
+          "name",
+          "author",
+          "genre",
+          "page count",
+          "published date",
+          "current page",
+          "reading status",
+        ]}
+      />
+      {selectedBook && (
+        <BookDetailsModal
+          open={openDetailsModal}
+          onClose={handleCloseDetailsModal}
+          book={selectedBook}
+          onUpdate={(updatedBook) => {
+            setBooks(
+              books.map((book) =>
+                book.id === updatedBook.id ? updatedBook : book
+              )
+            );
+          }}
         />
-        {selectedBook && (
-          <BookDetailsModal
-            open={openDetailsModal}
-            onClose={handleCloseDetailsModal}
-            book={selectedBook}
-            onUpdate={(updatedBook) => {
-              setBooks(
-                books.map((book) =>
-                  book.id === updatedBook.id ? updatedBook : book
-                )
-              );
-            }}
-          />
-        )}
-      </Box>
+      )}
+    </Box>
   );
 }
 
